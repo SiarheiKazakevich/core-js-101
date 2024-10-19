70c7f47031584ac82646e8f5f26f2ab73d22d735
Siarhei Kazakevich <dianrom@tut.by>
2024-10-19 20:53:13 +0300

    feat:add config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,46 @@
 module.exports = {
+	env: {
+		browser: true,
+		es6: true, // можно указать и es6, если es2021 не работает
+	},
+	parserOptions: {
+		ecmaVersion: 12, // 12 соответствует ECMAScript 2021
+		sourceType: 'module',
+	},
+	rules: {
+		// ваши правила
+	},
+};
+/*module.exports = {
   "env": {
-    "browser": true,
-    "es6": true,
-    "node": true
+	 "browser": true,
+	 "es6": true,
+	 "node": true
   },
   "extends": "eslint:recommended",
   "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+	 "Atomics": "readonly",
+	 "SharedArrayBuffer": "readonly"
   },
   "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+	 "ecmaVersion": 2018,
+	 "sourceType": "module"
   },
   "rules": {
   }
 };
+module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: "eslint:recommended",
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: "module",
+	},
+	rules: {
+		// Ваши правила здесь
+	},
+};
+*/
---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+export default [
+	{
+		ignores: ['node_modules/**'],
+	},
+	{
+		files: ['**/*.js'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+		},
+		rules: {
+			// Добавьте здесь ваши правила
+		},
+	},
+];
